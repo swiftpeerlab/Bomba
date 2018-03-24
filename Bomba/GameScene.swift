3c1a591cb0030e41147fdca5817f81cbb284b9c2
Tiago Martinho <martinho@thoughtworks.com>
2018-03-24 12:25:08 +0100

    Tiago & Mika | adding ninja assets and applying no rotating node

--- Bomba/GameScene.swift
+++ Bomba/GameScene.swift
@@ -7,7 +7,7 @@ class GameScene: SKScene {
     var graphs = [String : GKGraph]()
     
     private var lastUpdateTime : TimeInterval = 0
-    private var spinnyNode : SKShapeNode?
+    private var spinnyNode : SKSpriteNode?
     
     override func sceneDidLoad() {
 
@@ -15,29 +15,19 @@ class GameScene: SKScene {
         
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+//        let texture = SKTexture(
+        self.spinnyNode = SKSpriteNode(imageNamed: "Idle__000.png")
+
         
         if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
             spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        guard let n = spinnyNode?.copy() as? SKShapeNode else { return }
-        n.position = pos
-        n.strokeColor = SKColor.blue
+
+        guard let n = spinnyNode?.copy() as? SKSpriteNode else { return }
+        n.position = position
         addChild(n)
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
+
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
